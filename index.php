424be558616a6674909f975d51ca6fd0ac669bcf
Lipodat Artem <a.lipodat@mail.ru>
2022-09-20 16:42:16 +0300

    container

--- index.php
+++ index.php
@@ -8,148 +8,150 @@
       <div><img class="w-100" src="./image/slider_3.png" alt=""></div>
     </div>
   </section>
-  <section class="category d-flex gap-15px">
-    <a class="category_item d-flex w-50">
-      <div class="category_item_img w-50">
-        <img src="./image/category/001.png" alt="">
-      </div>
-      <div class="category_item_name w-50">
-        <span class="text-r pr-50px">Европейские роллы</span>
-      </div>
-    </a>
-    <a class="category_item d-flex w-50">
-      <div class="category_item_img w-50 order-2">
-        <img class="float-r border-radius-20px" src="./image/category/002.png" alt="">
-      </div>
-      <div class="category_item_name w-50 order-1">
-        <span class="pl-50px">Пицца</span>
-      </div>
-    </a>
-  </section>
-  <section class="categoty_columm d-flex gap-30px mt-30px h-730px">
-    <div class="w-33 d-flex column gap-30px">
-      <a class="category_column_item h-50">
-        <span class="text-c">Горячие роллы</span>
-        <div><img src="./image/category/003.png" alt=""></div>
+  <div class="container">
+    <section class="category d-flex gap-15px">
+      <a class="category_item d-flex w-50">
+        <div class="category_item_img w-50">
+          <img src="./image/category/001.png" alt="">
+        </div>
+        <div class="category_item_name w-50">
+          <span class="text-r pr-50px">Европейские роллы</span>
+        </div>
       </a>
-      <a style="background: #FDCACA;" class="category_column_item h-50">
-        <span class="pl-50px">Закуски</span>
-        <div class="d-flex j-c-c"><img src="./image/category/006.png" alt=""></div>
+      <a class="category_item d-flex w-50">
+        <div class="category_item_img w-50 order-2">
+          <img class="float-r border-radius-20px" src="./image/category/002.png" alt="">
+        </div>
+        <div class="category_item_name w-50 order-1">
+          <span class="pl-50px">Пицца</span>
+        </div>
       </a>
-    </div>
-    <div class="w-33 d-flex column">
-      <a class="category_column_item">
-        <span class="text-r pr-50px">Сеты</span>
-        <div class="d-flex j-c-c"><img src="./image/category/005.png" alt=""></div>
-      </a>
-    </div>
-    <div class="w-33 d-flex column gap-30px">
-      <a style="background: #CAD9FF;" class="category_column_item h-50">
-        <span class="text-c">Суши и гунканы</span>
-        <div class="d-flex j-c-c"><img src="./image/category/004.png" alt=""></div>
-      </a>
-      <a class="category_column_item h-50">
-        <span class="pl-50px">Напитки</span>
-        <div><img style="width: 230px; float:right;" src="./image/category/007.png" alt=""></div>
-      </a>
-    </div>
-  </section>  
-  <section class="delivery mt-50px">
-    <h3 class="h3">Условия доставки:</h3>
-    <div class="delivery_item d-flex mt-30px">
-      <div class="w-50">
-        <img src="./image/delivery.png" class="w-100" alt="">
+    </section>
+    <section class="categoty_columm d-flex gap-30px mt-30px h-730px">
+      <div class="w-33 d-flex column gap-30px">
+        <a class="category_column_item h-50">
+          <span class="text-c">Горячие роллы</span>
+          <div><img src="./image/category/003.png" alt=""></div>
+        </a>
+        <a style="background: #FDCACA;" class="category_column_item h-50">
+          <span class="pl-50px">Закуски</span>
+          <div class="d-flex j-c-c"><img src="./image/category/006.png" alt=""></div>
+        </a>
       </div>
-      <div class="w-50">
-        <p class="section-p arrow">Бесплатная доставка по городу при заказе от 550руб или от 150руб в зависимости от района Возможен самовывоз по адресу Карла Маркса 295</p>
+      <div class="w-33 d-flex column">
+        <a class="category_column_item">
+          <span class="text-r pr-50px">Сеты</span>
+          <div class="d-flex j-c-c"><img src="./image/category/005.png" alt=""></div>
+        </a>
       </div>
-    </div>
-  </section>
-  <section class="seels mt-50px">
-    <h3 class="h3">Актуальные акции:</h3>
-    <div class="seels_item d-flex mt-30px">
-      <div class="w-50">
-        <p class="section-p">Скидка 10% в честь дня рождения -30% на пиццу при заказе сета от 550руб</p>
+      <div class="w-33 d-flex column gap-30px">
+        <a style="background: #CAD9FF;" class="category_column_item h-50">
+          <span class="text-c">Суши и гунканы</span>
+          <div class="d-flex j-c-c"><img src="./image/category/004.png" alt=""></div>
+        </a>
+        <a class="category_column_item h-50">
+          <span class="pl-50px">Напитки</span>
+          <div><img style="width: 230px; float:right;" src="./image/category/007.png" alt=""></div>
+        </a>
       </div>
-      <div class="w-50">
-        <img src="./image/seels.png" class="w-100" alt="">
-      </div>
-    </div>
-  </section>
-  <section class="pizza mt-50px">
-    <div class="pizza_item">
-      <img src="./image/pizza.png" class="w-100" alt="">
-    </div>
-  </section>
-  <section class="advantages mt-50px">
-    <h3 class="h3">Наши преимущества:</h3>
-    <div class="advantages_item mt-30px pl-50px">
-      <div class="section-p arrow">Более 4х лет работы</div>
-      <div class="section-p arrow">Более 100тыс довольных клиентов</div>
-      <div class="section-p arrow">Адекватные цены</div>
-      <div class="section-p arrow">Контроль качества</div>
-    </div>
-    <div class="advantages_item">
-      <div class="w-50">
-        <img src="./image/advantages.png" class="w-100" alt="">
-      </div>
-    </div>
-  </section>
-  <section class="testimonials mt-50px">
-    <h3 class="h3">Отзывы:</h3>
-    <div class="testimonials_item d-flex mt-30px">
-      <div class="w-60">
-        <p class="section-p">Вы можете оставить свой отзыв в нашей группе в контакте, а так же увидеть последние новости, фото и видео</p>
-      </div>
-      <div class="w-40">
-        <img class="float-r" src="./image/vk.svg" alt="">
-      </div>
-    </div>
-  </section>
-  <section class="vk_testimonials mt-50px d-flex">
-    <div class="vk_testimonials_item">
-      <div class="person d-flex">
-        <div class="person-img">
-          <img src="./image/person.png" alt="">
+    </section>  
+    <section class="delivery mt-50px">
+      <h3 class="h3">Условия доставки:</h3>
+      <div class="delivery_item d-flex mt-30px">
+        <div class="w-50">
+          <img src="./image/delivery.png" class="w-100" alt="">
         </div>
-        <div class="person-name d-flex column">
-          <span class="section-p">Екатерина С.</span>
-          <span class="silver">02.24.21</span>
+        <div class="w-50">
+          <p class="section-p arrow">Бесплатная доставка по городу при заказе от 550руб или от 150руб в зависимости от района Возможен самовывоз по адресу Карла Маркса 295</p>
         </div>
       </div>
-      <div class="section-p text-c mt-30px">
-        Ваша доставка и ваши блюда лучшие в Ижевске!) всегда очень вкусно, вовремя, всегда вежливые курьеры и девушки на телефоне
-      </div>
-    </div>
-    <div class="vk_testimonials_item">
-      <div class="person d-flex">
-        <div class="person-img">
-          <img src="./image/person.png" alt="">
+    </section>
+    <section class="seels mt-50px">
+      <h3 class="h3">Актуальные акции:</h3>
+      <div class="seels_item d-flex mt-30px">
+        <div class="w-50">
+          <p class="section-p">Скидка 10% в честь дня рождения -30% на пиццу при заказе сета от 550руб</p>
         </div>
-        <div class="person-name d-flex column">
-          <span class="section-p">Екатерина С.</span>
-          <span class="silver">02.24.21</span>
+        <div class="w-50">
+          <img src="./image/seels.png" class="w-100" alt="">
         </div>
       </div>
-      <div class="section-p text-c mt-30px">
-        Ваша доставка и ваши блюда лучшие в Ижевске!) всегда очень вкусно, вовремя, всегда вежливые курьеры и девушки на телефоне
+    </section>
+    <section class="pizza mt-50px">
+      <div class="pizza_item">
+        <img src="./image/pizza.png" class="w-100" alt="">
       </div>
-    </div>
-    <div class="vk_testimonials_item">
-      <div class="person d-flex">
-        <div class="person-img">
-          <img src="./image/person.png" alt="">
+    </section>
+    <section class="advantages mt-50px">
+      <h3 class="h3">Наши преимущества:</h3>
+      <div class="advantages_item mt-30px pl-50px">
+        <div class="section-p arrow">Более 4х лет работы</div>
+        <div class="section-p arrow">Более 100тыс довольных клиентов</div>
+        <div class="section-p arrow">Адекватные цены</div>
+        <div class="section-p arrow">Контроль качества</div>
+      </div>
+      <div class="advantages_item">
+        <div class="w-50">
+          <img src="./image/advantages.png" class="w-100" alt="">
         </div>
-        <div class="person-name d-flex column">
-          <span class="section-p">Екатерина С.</span>
-          <span class="silver">02.24.21</span>
+      </div>
+    </section>
+    <section class="testimonials mt-50px">
+      <h3 class="h3">Отзывы:</h3>
+      <div class="testimonials_item d-flex mt-30px">
+        <div class="w-60">
+          <p class="section-p">Вы можете оставить свой отзыв в нашей группе в контакте, а так же увидеть последние новости, фото и видео</p>
+        </div>
+        <div class="w-40">
+          <img class="float-r" src="./image/vk.svg" alt="">
         </div>
       </div>
-      <div class="section-p text-c mt-30px">
-        Ваша доставка и ваши блюда лучшие в Ижевске!) всегда очень вкусно, вовремя, всегда вежливые курьеры и девушки на телефоне
+    </section>
+    <section class="vk_testimonials mt-50px d-flex">
+      <div class="vk_testimonials_item">
+        <div class="person d-flex">
+          <div class="person-img">
+            <img src="./image/person.png" alt="">
+          </div>
+          <div class="person-name d-flex column">
+            <span class="section-p">Екатерина С.</span>
+            <span class="silver">02.24.21</span>
+          </div>
+        </div>
+        <div class="section-p text-c mt-30px">
+          Ваша доставка и ваши блюда лучшие в Ижевске!) всегда очень вкусно, вовремя, всегда вежливые курьеры и девушки на телефоне
+        </div>
       </div>
-    </div>
-  </section>
+      <div class="vk_testimonials_item">
+        <div class="person d-flex">
+          <div class="person-img">
+            <img src="./image/person.png" alt="">
+          </div>
+          <div class="person-name d-flex column">
+            <span class="section-p">Екатерина С.</span>
+            <span class="silver">02.24.21</span>
+          </div>
+        </div>
+        <div class="section-p text-c mt-30px">
+          Ваша доставка и ваши блюда лучшие в Ижевске!) всегда очень вкусно, вовремя, всегда вежливые курьеры и девушки на телефоне
+        </div>
+      </div>
+      <div class="vk_testimonials_item">
+        <div class="person d-flex">
+          <div class="person-img">
+            <img src="./image/person.png" alt="">
+          </div>
+          <div class="person-name d-flex column">
+            <span class="section-p">Екатерина С.</span>
+            <span class="silver">02.24.21</span>
+          </div>
+        </div>
+        <div class="section-p text-c mt-30px">
+          Ваша доставка и ваши блюда лучшие в Ижевске!) всегда очень вкусно, вовремя, всегда вежливые курьеры и девушки на телефоне
+        </div>
+      </div>
+    </section>
+  </div>
 </main>
 
 <?php include_once('./template/footer.php'); ?>
